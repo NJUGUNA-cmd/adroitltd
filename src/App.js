8d1fea63272e4afc83c2cb17c56b48023a644b36
drookygames <38832368+drookygames@users.noreply.github.com>
2022-03-11 05:59:59 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <h1>ADROIT</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/feature/feature.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './feature.css';
+
+const feature = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default feature
